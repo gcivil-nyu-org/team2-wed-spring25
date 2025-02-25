54bcef72f468c4547588552b4dd6a54c6863eb4c
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-02-25 00:36:36 -0500

    updated settings.py added all hosts

--- backend/nightwalkers/nightwalkers/settings.py
+++ backend/nightwalkers/nightwalkers/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 # CORS settings
 CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_CORS_ALLOWED_ORIGINS', 'https://localhost:3000').split(',')
 CORS_ALLOW_CREDENTIALS = True
