4775a55b8013b04c6278c471db2e9102b828bd54
Alexander Sanchez <57728893+AlexanderS10@users.noreply.github.com>
2025-03-04 16:14:03 -0500

    Fixed backend files to match flake8 requirements

--- backend/nightwalkers/accounts/models.py
+++ backend/nightwalkers/accounts/models.py
@@ -1,42 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
 # Create your models here.
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password=None, **extra_fields):
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         if not first_name:
-            raise ValueError('Users must have a first name')
+            raise ValueError("Users must have a first name")
         if not last_name:
-            raise ValueError('Users must have a last name')
+            raise ValueError("Users must have a last name")
 
         email = self.normalize_email(email)
         user = self.model(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            **extra_fields
+            email=email, first_name=first_name, last_name=last_name, **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('email_verified', True)
-        extra_fields.setdefault('first_name', 'Admin')  # Default value for superuser
-        extra_fields.setdefault('last_name', 'User')  # Default value for superuser
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("email_verified", True)
+        extra_fields.setdefault("first_name", "Admin")  # Default value for superuser
+        extra_fields.setdefault("last_name", "User")  # Default value for superuser
         return self.create_user(email, password=password, **extra_fields)
+
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=150, blank=False, null=False)  # Make required
-    last_name = models.CharField(max_length=150, blank=False, null=False)   # Make required
+    first_name = models.CharField(
+        max_length=150, blank=False, null=False
+    )  # Make required
+    last_name = models.CharField(
+        max_length=150, blank=False, null=False
+    )  # Make required
     email_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -45,19 +48,21 @@ class User(AbstractUser):
 
     provider = models.CharField(max_length=50, blank=True)
     provider_id = models.CharField(max_length=100, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     avatar_url = models.URLField(blank=True, null=True, max_length=1024)
 
     objects = CustomUserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return self.email
 
     @property
     def get_avatar(self):
-        """Returns the user avatar either uploaded (if we end up supporting this but thought I would add this now) or for now OAuth avatar"""
+        """Returns the user avatar either uploaded
+        (if we end up supporting this but thought I would add this now)
+        or for now OAuth avatar"""
         if self.avatar:
             return self.avatar.url
         elif self.avatar_url:
--- backend/nightwalkers/nightwalkers/urls.py
+++ backend/nightwalkers/nightwalkers/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('', include('accounts.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),
+    path("", include("accounts.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()  # Add this
