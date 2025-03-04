cd1c92edae9b3595920d54d21481d21befbbbdd5
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-04 09:53:24 -0500

    fixed babel issue

--- frontend/babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    presets: [
-      '@babel/preset-env', // Transforms modern JavaScript to a compatible version
-      '@babel/preset-react', // Transforms JSX to JavaScript
-    ],
-  };
