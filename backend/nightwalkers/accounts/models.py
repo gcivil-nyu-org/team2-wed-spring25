46fc804e9375f002b7d595cfb7b46553355a21ee
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-04 15:29:58 -0500

    reformmated the code using black

--- backend/nightwalkers/accounts/models.py
+++ backend/nightwalkers/accounts/models.py
@@ -4,34 +4,30 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, first_name,
-                    last_name, password=None, **extra_fields):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         if not first_name:
-            raise ValueError('Users must have a first name')
+            raise ValueError("Users must have a first name")
         if not last_name:
-            raise ValueError('Users must have a last name')
+            raise ValueError("Users must have a last name")
 
         email = self.normalize_email(email)
         user = self.model(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            **extra_fields
+            email=email, first_name=first_name, last_name=last_name, **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('email_verified', True)
-        extra_fields.setdefault('first_name', 'Admin')
-        extra_fields.setdefault('last_name', 'User')
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("email_verified", True)
+        extra_fields.setdefault("first_name", "Admin")
+        extra_fields.setdefault("last_name", "User")
         return self.create_user(email, password=password, **extra_fields)
 
 
@@ -48,12 +44,12 @@ class User(AbstractUser):
 
     provider = models.CharField(max_length=50, blank=True)
     provider_id = models.CharField(max_length=100, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     avatar_url = models.URLField(blank=True, null=True, max_length=1024)
 
     objects = CustomUserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return self.email
