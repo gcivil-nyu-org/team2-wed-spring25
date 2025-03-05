a5c35bd50d720153384a531a136615c950f7fe40
nalinir <naliniramanathan@gmail.com>
2025-03-05 18:09:46 -0500

    Adjusted to centroid data

--- backend/nightwalkers/map/views.py
+++ backend/nightwalkers/map/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def road_view(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM filtered_grouped_data;")
+        cursor.execute("SELECT * FROM filtered_grouped_data_centroid;")
         rows = cursor.fetchall()
     return render(request, "my_template.html", {"data": rows})
