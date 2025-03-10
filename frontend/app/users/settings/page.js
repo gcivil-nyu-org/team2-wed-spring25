110595612b9cfcba6e8b642e3fc6789ecd0ecaf8
Damon Chu <dc3800@nyu.edu>
2025-03-09 23:44:54 -0400

    settings

---
+++ frontend/app/users/settings/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import SettingPanel from "@/app/custom-components/SettingPanel";
+
+const SettingsPage = () => {
+  return (
+    <>
+      <div>SettingsPage</div>
+      <SettingPanel />
+    </>
+  );
+};
+
+export default SettingsPage;
