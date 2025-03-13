08c147c932fb6fbe14faad28544388a36a42b4c6
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-12 23:57:22 -0400

    refactored UI code for like button post

--- frontend/components/molecules/LikeIconTextWithTooltip/LikeIconTextWithTooltip.jsx
+++ frontend/components/molecules/LikeIconTextWithTooltip/LikeIconTextWithTooltip.jsx
@@ -1,34 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
 import IconText from '@/components/molecules/IconText/IconText';
 import LikeOptionList from '@/components/molecules/LikeOptionList/LikeOptionList';
+import useLikeIconTextWithTooltip from './useLikeIconTextWithTooltip';
 
 export default function LikeIconTextWithTooltip({ iconData }){
-    const [isTooltipVisible, setTooltipVisible] = useState(false);
-    const hoverTimeoutRef = useRef(null); // Ref to store the timeout ID
-
-    const handleMouseEnter = () => {
-        // Clear any existing timeout to avoid hiding the tooltip prematurely
-        if (hoverTimeoutRef.current) {
-            clearTimeout(hoverTimeoutRef.current);
-        }
-        setTooltipVisible(true);
-    };
-
-    const handleMouseLeave = () => {
-        // Set a timeout to hide the tooltip after 0.5 seconds
-        hoverTimeoutRef.current = setTimeout(() => {
-            setTooltipVisible(false);
-        }, 100); // 500ms = 0.5 seconds
-    };
-
-    // Cleanup the timeout when the component unmounts
-    useEffect(() => {
-        return () => {
-            if (hoverTimeoutRef.current) {
-                clearTimeout(hoverTimeoutRef.current);
-            }
-        };
-    }, []);
+    const {
+        isTooltipVisible,
+        handleMouseEnter,
+        handleMouseLeave,
+    } = useLikeIconTextWithTooltip();
 
     return (
         <div
