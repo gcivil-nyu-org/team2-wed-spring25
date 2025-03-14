7d142c8587fe721d19b63cdfcca34f0b406764cb
nalinir <naliniramanathan@gmail.com>
2025-03-13 21:50:01 -0400

    Tests no longer relevant given Postgres instance

--- backend/nightwalkers/nightwalkers/settings.py
+++ backend/nightwalkers/nightwalkers/settings.py
@@ -123,7 +123,15 @@ DATABASES = {
         default=os.getenv("DJANGO_DATABASE_URL"),
         conn_max_age=600,  # Optional:
         # Improves performance by reusing connections
-    )
+    ),
+    "test":{
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
 }
 # Database
 # DATABASES = {
