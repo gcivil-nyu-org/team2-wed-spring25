c1871903dde30e8f2dbe5c98c7d359491b055616
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-17 20:16:52 -0400

    added throttle, ability to unlike, change like to diff type of like, updated comments and likes to have new comment and like to be inlucded when user creates it,

--- frontend/components/organisms/Forum/UserPost.jsx
+++ frontend/components/organisms/Forum/UserPost.jsx
@@ -3,8 +3,11 @@
 import UserPostBottom from "@/components/molecules/UserPost/UserPostBottom/UserPostBottom";
 import UserPostHeader from "@/components/molecules/UserPost/UserPostHeader/UserPostHeader";
 import UserPostBody from "@/components/molecules/UserPost/UserPostBody/UserPostBody";
+import useUserPost from "./useUserPost";
 
 export default function UserPost({ post }) {
+  const { commentsCount, setCommentsCount, likesCount, setLikesCount } =
+    useUserPost(post.likes_count, post.comments_count);
   return (
     <div className="flex flex-col rounded-lg w-full font-sans mb-2 bg-white border-[1px]">
       <UserPostHeader
@@ -15,8 +18,10 @@ export default function UserPost({ post }) {
       <UserPostBody image_urls={post.image_urls} content={post.content} />
       <UserPostBottom
         user_avatar={post.user_avatar}
-        comments_count={post.comments_count}
-        likes_count={post.likes_count}
+        commentsCount={commentsCount}
+        likesCount={likesCount}
+        setCommentsCount={setCommentsCount}
+        setLikesCount={setLikesCount}
         post_id={post.id}
         user_has_liked={post.user_has_liked}
         like_type={post.like_type}
