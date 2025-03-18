3b75903e3946be13382c93781b9ff5aae9dd8b55
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-18 09:11:49 -0400

    integrated damon2 latest to my local main

--- backend/nightwalkers/nightwalkers/urls.py
+++ backend/nightwalkers/nightwalkers/urls.py
@@ -28,9 +28,19 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # path("api/", include("accounts.urls")),
     path("api/", include("accounts.urls")),
+    path("", include("accounts.urls")),
     path("api/forum/", include("forum.urls")),
     path("forum/", include("forum.urls")),
     path("api/", include("map.urls")),
+]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),
+    path("", include("accounts.urls")),
+    path("api/forum/", include("forum.urls")),
+    path("forum/", include("forum.urls")),
+    path("api/", include("map.urls")),
+    path("", include("map.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()  # Add this
