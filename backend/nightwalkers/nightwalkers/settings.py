ac889c90f5053d608b9d974fc4935a7a8576f696
Damon Chu <dc3800@nyu.edu>
2025-03-18 09:33:45 -0400

    Team effort debug heatmap

--- backend/nightwalkers/nightwalkers/settings.py
+++ backend/nightwalkers/nightwalkers/settings.py
@@ -125,16 +125,6 @@ DATABASES = {
         # Improves performance by reusing connections
     )
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
