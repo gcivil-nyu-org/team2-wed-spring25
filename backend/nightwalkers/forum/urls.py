a005f2a5e2b14fe2a80dfc0bfe7cb0372714b06b
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-19 18:33:45 -0400

    fixed black and flake8 issues,

--- backend/nightwalkers/forum/urls.py
+++ backend/nightwalkers/forum/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
     # Like endpoints
     path("posts/<int:post_id>/like/", views.like_post, name="like_post"),
     path("posts/<int:post_id>/unlike/", views.unlike_post, name="unlike_post"),
-    path("posts/comments/<int:comment_id>/like/", views.like_comment, name="like_comment"),
+    path(
+        "posts/comments/<int:comment_id>/like/", views.like_comment, name="like_comment"
+    ),
     path(
         "posts/comments/<int:comment_id>/like/", views.like_comment, name="like_comment"
     ),
