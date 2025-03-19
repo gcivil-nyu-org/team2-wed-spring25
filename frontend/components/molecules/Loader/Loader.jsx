a97464b773a11858e30bfb7eca139d72b1ee3fe3
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-18 23:03:16 -0400

    fixed only image create post error, and loader unnecessary background image issue

--- frontend/components/molecules/Loader/Loader.jsx
+++ frontend/components/molecules/Loader/Loader.jsx
@@ -1,10 +1,10 @@
-import Icon from "@/components/atom/Icon/Icon";
+import Image from "next/image";
 
 export default function Loader({ size = "w-4 h-4", color = "text-gray-500" }) {
   return (
     <div className="flex flex-col justify-center items-center py-10">
-      <Icon src={"/owl-logo.svg"} width={40} height={40} alt={"Loading..."} />
-      <Icon
+      <Image src={"/owl-logo.svg"} width={40} height={40} alt={"Loading..."} />
+      <Image
         src={"/icons/fade-stagger-circles.svg"}
         width={40}
         height={40}
