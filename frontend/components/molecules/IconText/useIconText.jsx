48e3c169d133cc011c18af4470d37a7e3a968543
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-19 18:00:57 -0400

    added like option for comments, UI, and apis both, updated Comment, CommetnLike Model

--- frontend/components/molecules/IconText/useIconText.jsx
+++ frontend/components/molecules/IconText/useIconText.jsx
@@ -1,57 +1,5 @@
-import icons from "@/constants/icons";
-
-export default function useIconText(user_has_liked, like_type) {
-  const getLikeTypeColor = () => {
-    if (!user_has_liked) {
-      return "text-slate-600";
-    }
-    switch (like_type) {
-      case "Like":
-        return "text-blue-600";
-      case "Clap":
-        return "text-green-600";
-      case "Support":
-        return "text-purple-600";
-      case "Heart":
-        return "text-red-600";
-      case "Bulb":
-        return "text-yellow-600";
-      case "Laugh":
-        return "text-sky-600";
-      default:
-        return "text-slate-600";
-    }
-  };
-  const getIconSource = (src, user_has_liked, like_type) => {
-    if (user_has_liked) {
-      const icon = icons.find((icon) => icon.alt === like_type);
-      return icon ? icon.src : src;
-    }
-    return src;
-  };
-
-  const getGroupHoverTextColor = () => {
-    if (!user_has_liked) {
-      return "text-slate-900";
-    }
-    switch (like_type) {
-      case "Like":
-        return "text-blue-900";
-      case "Clap":
-        return "text-green-900";
-      case "Support":
-        return "text-purple-900";
-      case "Heart":
-        return "text-red-900";
-      case "Bulb":
-        return "text-yellow-900";
-      case "Laugh":
-        return "text-sky-900";
-      default:
-        return "text-slate-900";
-    }
-  };
+export default function useIconText() {
   const data = `w-15 h-15`;
 
-  return { getLikeTypeColor, getIconSource, getGroupHoverTextColor, data };
+  return { data };
 }
