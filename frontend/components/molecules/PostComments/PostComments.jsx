246e80219c9ba6f4611055748258cd94d54b3579
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-23 19:31:20 -0400

    edit post, edit comment, delete post, delete comment, complete.

--- frontend/components/molecules/PostComments/PostComments.jsx
+++ frontend/components/molecules/PostComments/PostComments.jsx
@@ -23,7 +23,6 @@ export default function PostComments({
     is_reply,
     parent_comment_id
   );
-  console.log("setComments from PostComments", setComments);
 
   return (
     <div className="flex flex-col mx-1 mb-2 ">
