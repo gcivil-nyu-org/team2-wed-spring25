a844759c1220742b4ea4ebba7fb3936552d24a58
Alexander Sanchez <57728893+AlexanderS10@users.noreply.github.com>
2025-03-24 14:59:18 -0400

    Merge

--- frontend/components/molecules/UserPost/UserPostHeader/UserPostHeader.jsx
+++ frontend/components/molecules/UserPost/UserPostHeader/UserPostHeader.jsx
@@ -6,7 +6,6 @@ import Icon from "@/components/atom/Icon/Icon";
 import CustomDialogBox from "@/components/organisms/CustomDialogBox/CustomDialogBox";
 import PostDialog from "@/components/organisms/PostDialog/PostDialog";
 
-<<<<<<< HEAD
 export default function UserPostHeader({
   user_avatar,
   user_fullname,
@@ -104,30 +103,6 @@ export default function UserPostHeader({
               <p>Delete</p>
             </li>
           </ul>
-=======
-export default function UserPostHeader({ user_avatar, user_fullname, date_created }) {
-    return (
-        <div className="flex flex-row px-4 pt-4">
-            <UserImage 
-                imageUrl={user_avatar} 
-                width={50} 
-                height={50}    
-            />
-            <div className="flex-1 flex-col justify-start items-start pl-3">
-                <p className="text-lg font-medium leading-none">
-                    {user_fullname}
-                </p>
-                <p className="text-sm font-thin text-gray-500 leading-none">
-                    Kingslayer
-                </p>
-                <p className="text-sm font-thin top-10 text-gray-500 leading-none">
-                    {formatDateAgo(date_created)}
-                </p>
-            </div>
-            <div>
-                <Button>+ Follow</Button>
-            </div>
->>>>>>> 9dc5cd8 (Complete UI for add comment input button, user post)
         </div>
       )}
       <UserImage imageUrl={user_avatar} width={48} height={48} />
