6a841e593521e1dd78582effa4da937e95f67736
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-04-01 20:05:53 -0400

    fixed pagination issue, updated loader,

--- frontend/components/organisms/Forum/UserPosts/UserPosts.jsx
+++ frontend/components/organisms/Forum/UserPosts/UserPosts.jsx
@@ -13,21 +13,6 @@ export default function UserPosts({
       {userPosts.map((post) => (
         <UserPost key={post.id} post={post} setPosts={setUserPosts} />
       ))}
-
-      {/* Always render the loader div, but hide it if there are no more posts */}
-      <div
-        ref={loaderRef}
-        className={`flex justify-center items-center h-[25vh] ${
-          hasMore ? "visible" : "hidden"
-        }`}
-      >
-        {isLoadingMore && <Loader />}
-      </div>
-      {!hasMore && (
-        <p className="text-center text-forum-heading2 text-lg mt-8 mb-10">
-          No more posts to show
-        </p>
-      )}
     </div>
   );
 }
